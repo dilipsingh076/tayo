29ec9e5fdf9be7f9bb01174f3d7ae92f5b48b6d7
dilipsingh076 <101004823+dilipsingh076@users.noreply.github.com>
2023-05-02 17:10:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dilip singh</h1>
+    </div>
+  );
+}
+
+export default App;
